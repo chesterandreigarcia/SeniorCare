aab9fc5ab99aee095d99a7eeecdcf18c435cf27c
Chester Andrei C. Garcia <161694981+chesterandreigarcia@users.noreply.github.com>
2026-08-30 02:16:14 +0800

    remove

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} /> */}
+        {/* <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<NotFound />} /> */}
       </Routes>
